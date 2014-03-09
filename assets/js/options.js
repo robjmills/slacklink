a670dfc6b9591a1df307107fff4575058545b8f2
Rob Mills <robjmills@gmail.com>
2014-03-09 13:12:21 +0000

    Initial commit of slacklink chrome extension

---
+++ assets/js/options.js
@@ -0,0 +1,34 @@
+// get values from local storage (if set) and populate form fields
+$(function() {
+  
+  var domain = localStorage.getItem('domain');
+  if( domain ) $('#domain').val(domain);
+
+  var token = localStorage.getItem('token');
+  if( token ) $('#token').val(token);
+
+  var channel = localStorage.getItem('channel');
+  if( channel ) $('#channel').val(channel);
+
+  var username = localStorage.getItem('username');
+  if( username ) $('#username').val(username);
+
+  var emoji = localStorage.getItem('emoji');
+  if( emoji ) $('#emoji').val(emoji);
+  
+});
+
+// store values in local storage on submit
+$( "#options-form" ).submit(function( event ) {
+
+  // stop form submitting
+  event.preventDefault();
+
+  // store values in local storage
+  localStorage.setItem('domain', $('#domain').val() );
+  localStorage.setItem('token', $('#token').val() );
+  localStorage.setItem('channel', $('#channel').val() );
+  localStorage.setItem('username', $('#username').val() );
+  localStorage.setItem('emoji', $('#emoji').val() );
+
+});
