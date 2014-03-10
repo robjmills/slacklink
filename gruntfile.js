2b1f16e4da3033f54b37f4680fa0f49f9454f4c6
Rob Mills <robjmills@gmail.com>
2014-03-10 13:37:24 +0000

    Build extension with Grunt

---
+++ gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-crx');
+
+    grunt.initConfig({
+      crx: {
+        myPublicPackage: {
+          "src": "src/",
+          "dest": "dist/crx/",
+          "exclude": [ ".git" ],
+          "privateKey": "key.pem"
+        }
+      }
+    });
+
+    grunt.registerTask('default', ['crx']);
+
+};
