283bbac472fe0a80b3b050097c7268f196764788
Rob Mills <robjmills@gmail.com>
2014-03-11 16:50:49 +0000

    Proper detection of active tab and unfurl links attempt 914

--- src/assets/js/slacklink.js
+++ src/assets/js/slacklink.js
@@ -1,39 +1,33 @@
 // slacklink.js - send current active URL to slack
-chrome.browserAction.onClicked.addListener(function(){
-    chrome.tabs.query({
-        active: true    
-    }, function(tabs) {
-        var tab = tabs[0];
-        url = tab.url;
-        
-        // grab config values from local storage
-        var domain = localStorage.getItem('domain');
-        var apitoken = localStorage.getItem('token');
-        var channel = localStorage.getItem('channel');
-        var username = localStorage.getItem('username');
-        var text = url;
-        var icon_emoji = localStorage.getItem('emoji');
+chrome.browserAction.onClicked.addListener(function(tab) {
+  // active tab URL
+  url = tab.url;  
+  // grab config values from local storage
+  var domain = localStorage.getItem('domain');
+  var apitoken = localStorage.getItem('token');
+  var channel = localStorage.getItem('channel');
+  var username = localStorage.getItem('username');
+  var text = url;
+  var icon_emoji = localStorage.getItem('emoji');
 
-        // add hash to channel if it's not there
-        if (channel.substring(0, 1) != "#") {
-            channel = "#" + channel;
-        }
+  // add hash to channel if it's not there
+  if (channel.substring(0, 1) != "#") {
+      channel = "#" + channel;
+  }
 
-        // payload for slack API request
-        var payload = {
-            channel: channel,
-            username: username,
-            text: url,
-            icon_emoji: icon_emoji,
-            unfurl_links: true
-        }
+  // payload for slack API request
+  var payload = {
+      channel: channel,
+      username: username,
+      text: url,
+      icon_emoji: icon_emoji,
+      unfurl_links: "true"
+  }
 
-        $.ajax({
-          type: "POST",
-          url: 'https://'+ domain +'/services/hooks/incoming-webhook?token='+apitoken,
-          data: JSON.stringify(payload),
-          dataType: 'json'
-        });
-    });
-
+  $.ajax({
+    type: "POST",
+    url: 'https://'+ domain +'/services/hooks/incoming-webhook?token='+apitoken,
+    data: JSON.stringify(payload),
+    dataType: 'json'
+  });
 });
