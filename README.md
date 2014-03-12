748b89a49de68acf1052a1aba8a6062fde7daf46
Rob Mills <robjmills@gmail.com>
2014-03-12 10:03:47 +0000

    Basic check for required fields added

--- README.md
+++ README.md
@@ -31,6 +31,7 @@ At the moment you won't be able to build this extension yourself without my priv
 Release History
 ========
 
+- 0.0.8 - Basic check for required fields added
 - 0.0.7 - Another attempt at forcing links to be expanded
 - 0.0.6 - Links should be expanded at Slack
 - 0.0.5 - Updated icon
--- src/assets/js/slacklink.js
+++ src/assets/js/slacklink.js
@@ -1,33 +1,46 @@
 // slacklink.js - send current active URL to slack
 chrome.browserAction.onClicked.addListener(function(tab) {
+  
   // active tab URL
   url = tab.url;  
+  
   // grab config values from local storage
+  
+  // required variables
   var domain = localStorage.getItem('domain');
   var apitoken = localStorage.getItem('token');
-  var channel = localStorage.getItem('channel');
-  var username = localStorage.getItem('username');
-  var text = url;
-  var icon_emoji = localStorage.getItem('emoji');
+  
+  if (!domain || !apitoken){ // Required variables are missing so show alert    
+    chrome.tabs.executeScript(tab.id, {code: "alert('Required variables are missing in your options page');"});
+  }else{ // all required variables are set    
+    // optional variables (will use default values if not set)
+    var channel = localStorage.getItem('channel');
+    channel = (channel) ? channel : '#random';
+    if (channel.substring(0, 1) != "#") {        
+        channel = "#" + channel; // add hash to channel if it's not there
+    }
 
-  // add hash to channel if it's not there
-  if (channel.substring(0, 1) != "#") {
-      channel = "#" + channel;
+    var username = localStorage.getItem('username');
+    username = (username) ? username : 'slacklink';  
+
+    var icon_emoji = localStorage.getItem('emoji');
+    icon_emoji = (icon_emoji) ? icon_emoji : ':slack:'; 
+
+    // payload for slack API request
+    var payload = {
+        channel: channel,
+        username: username,
+        text: url,
+        icon_emoji: icon_emoji,
+        unfurl_links: "true"
+    }
+
+    // make API request to slack
+    $.ajax({
+      type: "POST",
+      url: 'https://'+ domain +'/services/hooks/incoming-webhook?token='+apitoken,
+      data: JSON.stringify(payload),
+      dataType: 'json'
+    });
   }
-
-  // payload for slack API request
-  var payload = {
-      channel: channel,
-      username: username,
-      text: url,
-      icon_emoji: icon_emoji,
-      unfurl_links: "true"
-  }
-
-  $.ajax({
-    type: "POST",
-    url: 'https://'+ domain +'/services/hooks/incoming-webhook?token='+apitoken,
-    data: JSON.stringify(payload),
-    dataType: 'json'
-  });
 });
