53bf8248ed91672bea9a318ddfd5885807ca3cc7
Rob Mills <robjmills@gmail.com>
2014-04-22 17:29:52 +0100

    started work on contextual post message

--- src/assets/js/slacklink.js
+++ src/assets/js/slacklink.js
@@ -1,67 +1,85 @@
 // slacklink.js - send current active URL to slack
-chrome.browserAction.onClicked.addListener( function ( tab ) {
+$( "#popup-form" ).submit(function( event ) {
 
-	'use strict';
+   console.log('called');
 
-	// active tab URL
-	var url = tab.url;
-	
-	// grab config values from local storage
-	
-	// required variables
-	var domain = localStorage.getItem('domain');
-	var apitoken = localStorage.getItem('token');
-	
-	if ( !domain || !apitoken ) { // Required variables are missing so show alert
+  // stop form submitting
+  event.preventDefault();
 
-		chrome.tabs.create({ url: chrome.extension.getURL("options.html#required") });
-		
-	} else { // all required variables are set   
+  //post();
 
-		// optional variables (will use default values if not set)
-		var channel = localStorage.getItem('channel');
-		channel = (channel) ? channel : '#random';
-
-		if ( channel.substring(0, 1) !== '#' ) {
-			channel = '#' + channel; // add hash to channel if it's not there
-		}
-
-		var username = localStorage.getItem('username');
-		username = (username) ? username : 'slacklink';
-
-		var icon_emoji = localStorage.getItem('emoji');
-		icon_emoji = (icon_emoji) ? icon_emoji : ':slack:';
-
-		// payload for slack API request
-		var payload = {
-			channel: channel,
-			username: username,
-			text: url,
-			icon_emoji: icon_emoji,
-			unfurl_links: 'true'
-		};
-
-		// Insert our visual feedback dependencies
-		chrome.tabs.insertCSS( null, { file: 'assets/css/visualfeedback.css' });
-		chrome.tabs.executeScript( null, { file: 'assets/js/visualfeedback.js' });
-
-		// make API request to slack
-		$.ajax({
-
-			type: 'POST',
-			url: 'https://' + domain + '/services/hooks/incoming-webhook?token=' + apitoken,
-			data: JSON.stringify(payload)
-
-		}).always(function() {
-
-			// Tidy up our visual feedback dependencies
-			chrome.tabs.executeScript( null, { file: 'assets/js/tidy.js' });
-
-		}).fail(function(){
-
-			// Log error if post fails
-			console.error('Uh oh, something\'s wrong. Check your Slacklink options');
-
-		});
-	}
 });
+
+function post()
+{
+	console.log('so was i');
+
+	chrome.tabs.query({
+		active: true  
+	}, function(tabs) {
+
+		var tab = tabs[0];
+
+	    // active tab URL
+	    var url = tab.url;
+		
+		// grab config values from local storage
+		
+		// required variables
+		var domain = localStorage.getItem('domain');
+		var apitoken = localStorage.getItem('token');
+		
+		if ( !domain || !apitoken ) { // Required variables are missing so show alert
+
+			chrome.tabs.create({ url: chrome.extension.getURL("options.html#required") });
+			
+		} else { // all required variables are set   
+
+			// optional variables (will use default values if not set)
+			var channel = localStorage.getItem('channel');
+			channel = (channel) ? channel : '#random';
+
+			if ( channel.substring(0, 1) !== '#' ) {
+				channel = '#' + channel; // add hash to channel if it's not there
+			}
+
+			var username = localStorage.getItem('username');
+			username = (username) ? username : 'slacklink';
+
+			var icon_emoji = localStorage.getItem('emoji');
+			icon_emoji = (icon_emoji) ? icon_emoji : ':slack:';
+
+			// payload for slack API request
+			var payload = {
+				channel: channel,
+				username: username,
+				text: url,
+				icon_emoji: icon_emoji,
+				unfurl_links: 'true'
+			};
+
+			// Insert our visual feedback dependencies
+			chrome.tabs.insertCSS( null, { file: 'assets/css/visualfeedback.css' });
+			chrome.tabs.executeScript( null, { file: 'assets/js/visualfeedback.js' });
+
+			// make API request to slack
+			$.ajax({
+
+				type: 'POST',
+				url: 'https://' + domain + '/services/hooks/incoming-webhook?token=' + apitoken,
+				data: JSON.stringify(payload)
+
+			}).always(function() {
+
+				// Tidy up our visual feedback dependencies
+				chrome.tabs.executeScript( null, { file: 'assets/js/tidy.js' });
+
+			}).fail(function(){
+
+				// Log error if post fails
+				console.error('Uh oh, something\'s wrong. Check your Slacklink options');
+
+			});
+		}
+	});
+}
